f7b08e17a64bb59e08281a30df76ed906f9b767d
varshabhijeet <47491842+varshabhijeet@users.noreply.github.com>
2020-05-28 19:15:48 +0530

    automation

---
+++ kubernetes script/gcp.sh
@@ -0,0 +1,26 @@
+#Download and install Google Cloud SDK
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-294.0.0-linux-x86_64.tar.gz
+tar zxvf  google-cloud-sdk-294.0.0-linux-x86_64.tar.gz
+
+./google-cloud-sdk/install.sh
+PATH="google-cloud-sdk/bin:${PATH}"
+
+
+# install kubectl 
+gcloud components install kubectl
+
+
+#Activate service account 
+gcloud auth activate-service-account service-key@river-span-272204.iam.gserviceaccount.com --key-file /root/key/key.json
+
+
+#Update gcloud component
+gcloud components update
+
+gcloud init
+
+
+
+#run script
+bash variable.sh
+

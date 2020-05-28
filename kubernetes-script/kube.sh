09ddd322e126a082080aa66245055e23cfebff2d
varshabhijeet <47491842+varshabhijeet@users.noreply.github.com>
2020-05-28 19:21:44 +0530

    Automation

---
+++ kubernetes-script/kube.sh
@@ -0,0 +1,2 @@
+gcloud container clusters create "$cluster_name" --zone="$cluster_zone" --num-nodes="$node_count" --machine-type="$machine_type"
+
---
+++ kubernetes-script/variable.sh
@@ -0,0 +1,9 @@
+export cluster_name="testcluster1"
+export cluster_zone=us-central1-a
+#export add_zone=us-central1-b,us-central1-c
+export node_count="2"
+export machine_type="n1-standard-2"
+
+
+bash kube.sh
+
